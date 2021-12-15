7c4bebd8b36cf0d1c81cebd556e752604a5e1ab8
rst-ortega <sergio.ortega@rstcom.com.br>
2021-12-14 23:41:17 -0300

    primeira página

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const { defaultConfig } = require("next/dist/server/config-shared")
+
+//componente react
+function Home() {
+    return <div>Home 2</div>
+}
+
+//exportando por padrão qual componente eu quero que seja reinderizado como página
+export default Home
