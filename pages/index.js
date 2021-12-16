e2eb0f0c37d6ece762327c48a3d05d65ff6c2f59
rst-ortega <sergio.ortega@rstcom.com.br>
2021-12-16 08:45:26 -0300

    alteração de texto da msg 2

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const { defaultConfig } = require("next/dist/server/config-shared")
 
 //componente react
 function Home() {
-    return <div>Site em construção <br>Aguarde! </div>
+    return <div>Site em construção</div>
 }
 
 //exportando por padrão qual componente eu quero que seja reinderizado como página
