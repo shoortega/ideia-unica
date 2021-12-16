5c938c49dca3d1874bec9a11785709687190a055
rst-ortega <seh.ortega@gmail.com>
2021-12-16 12:37:11 -0300

    Criação de Sobre e implementação de uso do componente LINK

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,14 +1,15 @@
-import Link from "next/link";
+import Link from "next/link" ;
 
-function Home() {
+function Sobre() {
     return (<div>
         <h1>Site em construção!</h1>
-        
-        <Link href="/sobre">
-            <a> Acessar página Sobre </a>        
+
+        <Link href="/">
+            <a>Voltar para  Home</a> 
         </Link>
+        
     </div>)
 }
 
 //exportando por padrão qual componente eu quero que seja reinderizado como página
-export default Home
+export default Sobre
