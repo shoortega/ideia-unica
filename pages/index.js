c41c4251999e8ade8956b218cb63ad7147884995
rst-ortega <seh.ortega@gmail.com>
2021-12-16 12:46:15 -0300

    Test Link

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function Home() {
     return (<div>
-        <h1>Site em construção!</h1>
+        <h1>Home</h1>
         
         <Link href="/sobre">
             <a> Acessar página Sobre </a>        
--- pages/sobre.js
+++ pages/sobre.js
@@ -2,7 +2,7 @@ import Link from "next/link" ;
 
 function Sobre() {
     return (<div>
-        <h1>Site em construção!</h1>
+        <h1>Sobre</h1>
 
         <Link href="/">
             <a>Voltar para  Home</a> 
